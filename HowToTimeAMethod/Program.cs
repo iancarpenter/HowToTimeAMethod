efb942168ff1745a49eaa222db3291acf72517cc
Ian Carpenter <ianhcarpenter@gmail.com>
2017-11-25 21:48:18 +0000

    Initial Commit

--- HowToTimeAMethod/Program.cs
+++ HowToTimeAMethod/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HowToTimeAMethod
 {
     class Program
     {
+        /// <summary>
+        /// Find out out long a method takes to execute
+        /// Will form the basis of a blog post
+        /// </summary>
+        
         static void Main(string[] args)
         {
+            var watch = Stopwatch.StartNew();
+
+            for(int i = 0; i < 1000000; i++)
+            {
+                // do nothing
+            }
+
+            watch.Stop();
+
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Elapsed ms: {elapsedMs}");
+
+            var elapsed = watch.Elapsed;
+            Console.WriteLine($"Elapsed: {elapsed}");
         }
     }
 }
